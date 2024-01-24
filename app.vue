6281552a8b897546823c5c00d7582f4006d8dc84
Pascal <79517800+Pascale-Cheddar@users.noreply.github.com>
2024-01-24 21:04:05 +0100

    Add composable to add to todo in firebase  (#3)
    * Add parameter to useAddTodo function
    
    * Add functionality to add new todo and todo types
    
    * Fix useAddTodo function and update package dependencies

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <script setup lang="ts">
 
+import type { Todo } from '~/types/todo';
 
+
+let newTodo: Todo = {
+id: '1',
+title: 'new todo',
+completed: false,
+priority: 1,
+createdAt: new Date(),
+updatedAt: new Date(),
+};
+
+const addTodo = async(todo: Todo) => {
+ try {
+  const response = await useAddTodo(todo)
+ } catch (error) {
+   console.log(error)
+ }
+
+};
+
+addTodo(newTodo);
 </script>
 
 <style>
---
+++ composables/useAddTodo.ts
@@ -0,0 +1,20 @@
+import { addDoc, collection } from 'firebase/firestore';
+import type { Todo } from '~/types/todo';
+import { v4 as uuid } from 'uuid';
+const id = uuid();
+
+export const useAddTodo = async (todo: Todo) => {
+  const db = useFirestore();
+  try {
+    const ref = collection(db, 'todos');
+    await addDoc(ref, {
+      ...todo,
+      uuid: id,
+    });
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.log(error);
+  }
+  // eslint-disable-next-line no-console
+  console.log('useAddTodo');
+};
