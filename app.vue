8af9bfa69886a41519c3da663979a750265939cc
Pascal <pascale.koop@live.com>
2024-01-26 15:29:07 +0100

    Remove unused code

--- app.vue
+++ app.vue
@@ -1,28 +1,13 @@
 <script setup lang="ts">
-import type { Todo } from '~/types/todo';
-let newTodo: Todo = {
-id: '1',
-title: 'new todo',
-completed: false,
-priority: 1,
-createdAt: new Date(),
-updatedAt: new Date(),
-};
-
-// watchEffect(() => {
-//   onAuthStateChanged(auth, (user) => {
-//     if (user) {
-//       userid.value = user.uid;
-//       navigateTo('/')
-//     } else {
-//       userid.value = null;
-//       navigateTo('/login')
-//     }
-//   });
-
-// })
-
-
+// import type { Todo } from '~/types/todo';
+// let newTodo: Todo = {
+// id: '1',
+// title: 'new todo',
+// completed: false,
+// priority: 1,
+// createdAt: new Date(),
+// updatedAt: new Date(),
+// };
 
 // const addTodo = async(todo: Todo) => {
 //  try {
@@ -31,18 +16,6 @@ updatedAt: new Date(),
 //    console.log(error)
 //  }
 // };
-
-  // const signout = async() => {
-  //   try {
-  //     const response = await useSignOut()
-  //     // if (response) {
-  //     //   navigateTo('/login')
-  //     // }
-  //   } catch (error) {
-
-  //     console.log(error)
-  //   }
-  // };
 
 </script>
 
