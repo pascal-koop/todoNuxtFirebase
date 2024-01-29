13e4a074cc000a430161f3810c711a9296257f90
Pascal <pascale.koop@live.com>
2024-01-29 08:57:44 +0100

    Add @pinia/nuxt module to nuxt.config.ts and package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', 'nuxt-vuefire'],
+  modules: ['@nuxtjs/tailwindcss', 'nuxt-vuefire', '@pinia/nuxt'],
 
   vuefire: {
     config: {
